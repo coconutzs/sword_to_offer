3ce4b2e7c53f23c4e7396280fba578cfcb4c8680
coconutzs <coconutzs329@gmail.com>
2018-11-05 21:43:13 +0800

    update3_18.11.05

---
+++ Java/hello.java
@@ -0,0 +1,7 @@
+class Hello{
+    static final double PI = 3.1415927;
+    public static void main(String[] args){
+        System.out.println(PI);
+        System.out.println("Hello world!");
+        }
+    }
